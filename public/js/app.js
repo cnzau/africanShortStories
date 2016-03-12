f0956d3e1dce726b03f8476ba363f669cca350cf
cnzau <clement.nzau@gmail.com>
2016-03-12 21:34:57 +0300

    modified main controller of our app

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,4 @@
 // name our angular app
 angular.module('shortStories', ['routerRoutes'])
 .controller('mainController', function() {
-	// bind this to vm (view-model)
-	var vm = this;
-	// define variables and objects on this
-	// this lets them be available to our views
-	// define a basic variable
-	vm.message = 'Hey there! Welcome to our stories page!';
 });
