2727b62d100cc01ee6dc6e806658131032c4365d
cnzau <clement.nzau@gmail.com>
2016-03-13 10:54:25 +0300

    function to list all files in stories folder

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,26 @@
 // name our angular app
 angular.module('shortStories', ['routerRoutes'])
 .controller('mainController', function() {
+})
+.controller('storiesController', function() {
+	var dir = '../views/pages';
+	var _getAllFilesFromFolder = function(dir) {
+
+	    var filesystem = require("fs");
+	    var results = [];
+
+	    filesystem.readdirSync(dir).forEach(function(file) {
+
+	        file = dir+'/'+file;
+	        var stat = filesystem.statSync(file);
+
+	        if (stat && stat.isDirectory()) {
+	            results = results.concat(_getAllFilesFromFolder(file))
+	        } else results.push(file);
+
+	    });
+
+	    return results;
+
+	};
 });
